61c64da2eb4ecc8384385f2828d497901bf72b2e
Shahid Gillani <shahid5ssg@gmail.com>
2024-07-19 19:19:24 +0500

    next-middlewares added

---
+++ next-middleware/middleware.ts
@@ -0,0 +1,36 @@
+import { NextResponse } from 'next/server'
+import type { NextRequest } from 'next/server'
+
+let requestCount = 0;
+export function middleware(request: NextRequest) {
+  requestCount++;
+  console.log("number of requests is " + requestCount);
+  return  NextResponse.next()
+}
+
+
+// Selectively running middlewares: it's weired may be fixed in future
+ 
+// export function middleware(request: NextRequest) {
+//   console.log(request.nextUrl.pathname)
+//   if (request.nextUrl.pathname.startsWith('/admin')) {
+//     return NextResponse.redirect(new URL('/signin', request.url))
+//   }
+ 
+//   if (request.nextUrl.pathname.startsWith('/dashboard')) {
+//     return NextResponse.next()
+//   }
+// }
+
+
+
+
+
+
+
+
+
+// See "Matching Paths" below to learn more
+export const config = {
+  matcher: '/api/:path*',
+}
